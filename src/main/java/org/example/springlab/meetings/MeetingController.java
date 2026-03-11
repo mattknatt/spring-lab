4fd3f33ce335aa03a3dfbc6ea14b89ac78ea7971
mattknatt <mattiashagstrommusic@gmail.com>
2026-03-11 13:08:47 +0100

    Adds MeetingController with method for viewing booked meetings and meetings.html template.

---
+++ src/main/java/org/example/springlab/meetings/MeetingController.java
@@ -0,0 +1,25 @@
+package org.example.springlab.meetings;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+public class MeetingController {
+
+    private final MeetingService meetingService;
+    public MeetingController(MeetingService meetingService) {
+        this.meetingService = meetingService;
+    }
+    @GetMapping("/meetings")
+    public String viewMeetings(Model model) {
+        List<Meeting> meetings = meetingService.getAllMeetings();
+        model.addAttribute("meetings", meetings);
+        return "meetings";
+    }
+
+
+}
--- src/main/java/org/example/springlab/meetings/MeetingRepository.java
+++ src/main/java/org/example/springlab/meetings/MeetingRepository.java
@@ -1,10 +1,12 @@
 package org.example.springlab.meetings;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface MeetingRepository extends ListCrudRepository<Meeting, Long> {
     List<Meeting> findByDate(LocalDate date);
     List<Meeting> findByRoomId(Long roomId);
