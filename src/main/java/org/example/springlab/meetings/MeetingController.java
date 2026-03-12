b6b95075f53659fbc2abbe1c23d4e4fb2ea110fa
mattknatt <mattiashagstrommusic@gmail.com>
2026-03-12 12:03:52 +0100

    Hides table when no meetings exist. Removed unused import.

--- src/main/java/org/example/springlab/meetings/MeetingController.java
+++ src/main/java/org/example/springlab/meetings/MeetingController.java
@@ -3,7 +3,6 @@ package org.example.springlab.meetings;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
